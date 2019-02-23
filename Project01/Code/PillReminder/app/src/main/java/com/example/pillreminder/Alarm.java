5eca0c5553576d5b988a99cf778f07d1e2cb431e
Asik Azad <asik.azad@northsouth.edu>
2019-02-24 03:49:10 +0600

    Initial code base of medicine reminder app

---
+++ Project01/Code/PillReminder/app/src/main/java/com/example/pillreminder/Alarm.java
@@ -0,0 +1,42 @@
+package com.example.pillreminder;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class Alarm extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        // Get id & message from intent.
+        int notificationId = intent.getIntExtra("notificationId", 0);
+        String message = intent.getStringExtra("todo");
+
+        // When notification is tapped, call Reminder activity.
+        Intent mainIntent = new Intent(context, Reminder.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context,
+                0, mainIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationManager myNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Prepare notification.
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("It's time to take your pills !")
+                .setContentText(message)
+                .setWhen(System.currentTimeMillis())
+                .setAutoCancel(true)
+                .setContentIntent(contentIntent)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_ALL);
+
+        // Notify
+        myNotificationManager.notify(notificationId, builder.build());
+
+    }
+}
---
+++ Project01/Code/PillReminder/app/src/main/java/com/example/pillreminder/Reminder.java
@@ -0,0 +1,81 @@
+package com.example.pillreminder;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class Reminder extends AppCompatActivity implements View.OnClickListener{
+
+    private int notificationId = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder);
+
+        // Set Onclick Listener.
+        findViewById(R.id.setbutton).setOnClickListener(this);
+        findViewById(R.id.cancelbutton).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText editText = findViewById(R.id.medicinename);
+        TimePicker timePicker = findViewById(R.id.timepicker);
+
+        // Set notificationId & text.
+        Intent intent = new Intent(this, Alarm.class);
+        intent.putExtra("notificationId", notificationId);
+        intent.putExtra("todo", editText.getText().toString());
+
+        // getBroadcast(context, requestCode, intent, flags)
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(this, 0,
+                intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        AlarmManager alarm = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        switch (view.getId()) {
+            case R.id.setbutton:
+                int hour = timePicker.getCurrentHour();
+                int minute = timePicker.getCurrentMinute();
+
+                // Create time.
+                Calendar startTime = Calendar.getInstance();
+                startTime.set(Calendar.HOUR_OF_DAY, hour);
+                startTime.set(Calendar.MINUTE, minute);
+                startTime.set(Calendar.SECOND, 0);
+                long alarmStartTime = startTime.getTimeInMillis();
+
+                // Set alarm.
+                // set(type, milliseconds, intent)
+                alarm.setRepeating(AlarmManager.RTC_WAKEUP, alarmStartTime,
+                        AlarmManager.INTERVAL_DAY,alarmIntent);
+                notificationId++;
+                Toast.makeText(this, "Done!" , Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.cancelbutton:
+                alarm.cancel(alarmIntent);
+                Toast.makeText(this, "Canceled.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            finish();
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
+    }
+}
